203bc1971f7244e6a00700e21c8ecfbcaef03640
Alex Mastin <cd3770rn@go.minneapolis.edu>
2018-09-06 19:06:31 -0500

    First commit

---
+++ dir/part2.py
@@ -0,0 +1,25 @@
+def titlecase(word):
+    word = word.lower()
+    word = word.capitalize()
+    return word
+
+def camelCase():
+    toParse = input("Enter a senstence to parse to Camel Case: ")
+    words = toParse.split(" ")
+    parsed = ""
+    for word in words:
+
+        # All words that are not the first word should be capitalized
+        if words.index(word) != 0:
+            word = word.lower()
+            word = titlecase(word)
+
+        # The first word should be lowercase
+        else:
+            word = word.lower()
+
+        parsed += word
+
+    print(parsed)
+
+camelCase()
